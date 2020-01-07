e3b55d49290c25bbb7498ca3cfc14a43be127db2
pmuchiri <pmuchiri@cytonn.com>
2020-01-06 23:22:25 -0500

    first commit

---
+++ main2.js
@@ -0,0 +1,12 @@
+// The root Vue instance
+new Vue({
+    data: {
+      foo: 1
+    },
+    computed: {
+      bar: function () { /* ... */ }
+    },
+    methods: {
+      baz: function () { /* ... */ }
+    }
+  })
---
+++ main2v1.js
@@ -0,0 +1,11 @@
+// Get root data
+this.$root.foo
+
+// Set root data
+this.$root.foo = 2
+
+// Access root computed properties
+this.$root.bar
+
+// Call root methods
+this.$root.baz()
